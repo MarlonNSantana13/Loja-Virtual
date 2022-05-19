4785f8b5a740a440e2936366983e1aa6990ebd47
Marlon <srduelo13@gmail.com>
2022-05-19 19:20:17 -0300

    Update home.js

--- public/js/home/home.js
+++ public/js/home/home.js
@@ -2,7 +2,10 @@ import { users } from '../services/service.js'
 
 users.get().then(snapshot => {
 
+   
     console.log(snapshot.docs)
+    console.log(snapshot.docs)
+    
     console.log(user.uid);
 
 
