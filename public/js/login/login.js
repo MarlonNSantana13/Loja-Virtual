1568a2866d8df482d21c5afec7b7cffff4cbdcc1
Marlon <srduelo13@gmail.com>
2022-05-19 22:58:36 -0300

    Update login.js

--- public/js/login/login.js
+++ public/js/login/login.js
@@ -5,9 +5,6 @@ btnLogin.addEventListener('click', (e) => {
 
     var emaillogin = document.getElementById('inputEmail').value;
     var senhalogin = document.getElementById('inputSenha').value;
-    console.log(emaillogin);
-    console.log(senhalogin);
-
 
     firebase.auth().signInWithEmailAndPassword(emaillogin, senhalogin)
   .then((userCredential) => {
@@ -15,12 +12,16 @@ btnLogin.addEventListener('click', (e) => {
     
     // Signed in
     var user = userCredential.user;
+
+    // enviar user pro home
+
+
+
     console.log("UID : " +user.uid);
     console.log("Email : " +user.email);
-    console.log("Name : " +user.name);
-  
+
       alert("Senha Certa");
-      window.location.href='/home.html';  
+      window.location.href='/public/home.html';  
   })
   .catch((error) => {
     var errorCode = error.code;
