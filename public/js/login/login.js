fb001d4fde387eb26005812db8140d833047ac2a
Marlon <srduelo13@gmail.com>
2022-05-20 21:06:01 -0300

    0.3.1

--- public/js/login/login.js
+++ public/js/login/login.js
@@ -9,20 +9,14 @@ btnLogin.addEventListener('click', (e) => {
     firebase.auth().signInWithEmailAndPassword(emaillogin, senhalogin)
   .then((userCredential) => {
 
-
-    // Signed in
     var user = userCredential.user;
     var levar = user.uid;
 
-    //new
     setCookie("useruid", levar); //criar cookie com o uid do user logado
-      //alert("Senha Certa");
-    //  window.location.href = '/home.html';
-    //new
-
-    console.log("UID : " +user.uid);
-    console.log("Email : " +user.email);
-    console.log("Name : " +user.name);
+    
+  //console.log("UID : " +user.uid);
+  //console.log("Email : " +user.email);
+  // console.log("Name : " +user.name);
   
       alert("Senha Certa");
       window.location.href='/home.html';  
@@ -33,39 +27,6 @@ btnLogin.addEventListener('click', (e) => {
     alert("Email ou Senha Inválido !");
   });
 });
-
-/*
-var btnLogin = document.getElementById('btnLogin');
-
-btnLogin.addEventListener('click', (e) => {
-
-  var emaillogin = document.getElementById('inputEmail').value;
-  var senhalogin = document.getElementById('inputSenha').value;
-
-  console(emaillogin);
-  console(senhalogin);
-
-  firebase.auth().signInWithEmailAndPassword(emaillogin, senhalogin)
-    .then((userCredential) => {
-      // Signed in
-      var user = userCredential.user;
-
-      setCookie("useruid", user.uid); //criar cookie com o uid do user logado
-
-      alert("Senha Certa");
-      window.location.href = '/home.html';
-
-
-      console.log("UID : " +user.uid);
-      console.log("Email : " +user.email);
-    })
-    .catch((error) => {
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      alert("Email ou Senha Inválido !");
-    });
-});*/
-
 
 
 
